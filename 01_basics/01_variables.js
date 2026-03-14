16d5508f620d95f10ad4bb3e0f73e834f7ffe64f
anujkheterpal2009 <anujkheterpal2009@gmail.com>
2026-03-14 15:27:35 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountID = 14453
+let accountEmail = "anuj@google.com"
+var accountPassword = "12345"
+accountCity = "Delhi"
+let accountstate
+//fffef//
+
+console.log(accountID);
+console.table([accountID, accountEmail, accountPassword, accountCity, accountstate])
+/*
+hehe
+*/
