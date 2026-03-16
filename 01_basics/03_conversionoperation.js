bd8f5549451d3f7c1e5d78ead584a3862e4fcad0
anujkheterpal2009 <anujkheterpal2009@gmail.com>
2026-03-16 17:15:16 +0000

    conversion operation

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -22,5 +22,28 @@ console.log(2+2)
 console.log(2*2)
 console.log(2-2)
 console.log(2/2)
+console.log(2**9)
+
+let str1=44
+let str2=56
+let str3=str1+str2
+console.log(str3)
+
+str4=55
+
+str5=46
+str6=74
+str7=4657
+str8=6573
+str9=653
+str10=653
+str11=765
+str12=657
+str13=33
+str14=89
+
+str15=str4*str5*str6*str7+str8*str9+str10+str11*str12*str13*str14
+console.log(str15)
 
 
+console.log(100000000000*999999999999999999999999999999999999999)
